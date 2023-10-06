a26a5c8e68380a69608d96cb376b9da64249c839
Mohamad El-Haroufi <116694329+moe-e@users.noreply.github.com>
2023-10-06 11:18:03 -0400

    Time: 4736 ms (9.75%), Space: 20.3 MB (91.04%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,38 +1,27 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
         nums.sort()
-        first = []
-        possible = []
         
-        for i in range(len(nums)):
-            if nums[i] in first:
+        for i in range(0, len(nums)-2):
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
-            first.append(nums[i])
-            
-            l = i+1
+                
+            l = i + 1
             r = len(nums) - 1
-            
+                        
             while l < r:
-                ThreeSum = nums[i] + nums[l] + nums[r]
                 
-                if ThreeSum > 0:
-                    r-=1
-                elif ThreeSum < 0:
+                if nums[i] + nums[l] + nums[r] > 0:
+                    r -= 1
+
+                elif nums[i] + nums[l] + nums[r] < 0:
                     l+=1
-                    
+
                 else:
-                    possible.append([nums[i],nums[l],nums[r]])
-                    l+=1
-                    r-=1
-                    while l < r and nums[l] == nums[l - 1]:
-                        l+=1
-                    while l < r and nums[r] == nums[r+1]:
-                        r -=1
-                    
-        return possible
-                    
-                
-                    
-            
-        
+                    if [nums[i],nums[l],nums[r]] not in res:
+                        res.append([nums[i],nums[l],nums[r]])
+                    l += 1
+
+        return res
         
