0514a101c8e3bfe6423f2116465b5a7db42d97c1
Mohamad El-Haroufi <116694329+moe-e@users.noreply.github.com>
2024-02-21 20:06:39 -0500

    Time: 1683 ms (27.22%), Space: 17.5 MB (80.82%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        table = {}
-
-        for x in range(len(nums)):
-            needed = target - nums[x]
-            
-            if needed in table:
-                return [table[needed],x]
-            
-            table[nums[x]] = x
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
